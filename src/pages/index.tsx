90c7b694dd3379494d17ae46081501ac15feb12d
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2024-02-05 22:50:27 -0300

    feat: add header in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import { Header } from '@/components/organisms/Header'
-import { useTheme } from 'next-themes'
 import { Poppins } from 'next/font/google'
 import Head from 'next/head'
 import { useEffect, useState } from 'react'
@@ -8,8 +7,6 @@ const poppins = Poppins({ weight: '400', subsets: ['latin'] })
 
 export default function Home() {
   const [mounted, setMounted] = useState(false)
-
-  const { theme, setTheme, resolvedTheme } = useTheme()
 
   useEffect(() => {
     setMounted(true)
@@ -24,26 +21,7 @@ export default function Home() {
       <Head>
         <title>Cybera</title>
       </Head>
-      <Header>
-        <h1>Logo</h1>
-        <nav>
-          <ul>
-            <li>Home</li>
-            <li>Sobre</li>
-            <li>Contato</li>
-          </ul>
-        </nav>
-        <div>
-          <button
-            onClick={() => {
-              setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
-            }}
-            className="text-black dark:text-white"
-          >
-            {theme === 'dark' ? 'Light Mode' : 'Dark Mode'}
-          </button>
-        </div>
-      </Header>
+      <Header />
       <main className={`min-h-screen p-14 ${poppins.className}`}>
         <h1>Fonte Poppins</h1>
       </main>
